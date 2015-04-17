8cbb173a0010bce57640e8e20a219f636d6dd1be
Dmitry Grigoriev <stonger@yandex.ru>
2015-04-17 16:36:52 +0300

    Date

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -4,9 +4,19 @@ import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.index;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Application extends Controller {
 
   public static Result index() {
     return ok(index.render("Hi"));
+  }
+
+  public static String getDate() {
+    DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+    Date date = new Date();
+    return dateFormat.format(date);
   }
 }
