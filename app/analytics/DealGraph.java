6c7041ccca4200d1dc692c438f34f9b324a18be6
Dmitry Grigoriev <stonger@yandex.ru>
2015-04-21 17:48:13 +0300

    Graphs

---
+++ app/analytics/DealGraph.java
@@ -0,0 +1,19 @@
+package analytics;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * <p></p>
+ *
+ * @author Dmitriy Grigoriev
+ */
+public class DealGraph {
+  public static Map<Long, String> getGeneralInfo() {
+    Map<Long, String> statistic = new HashMap<>();
+
+
+
+    return null;
+  }
+}
