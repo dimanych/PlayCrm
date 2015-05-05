c38a0fe9967d58ebc62be8bfab247796a9e13259
Dmitry Grigoriev <stonger@yandex.ru>
2015-05-05 15:15:16 +0300

    Заказы обновление

--- app/utils/Util.java
+++ app/utils/Util.java
@@ -1,5 +1,8 @@
 package utils;
 
+import models.BaseModel;
+import org.apache.commons.lang3.StringUtils;
+
 import java.math.BigDecimal;
 import java.text.DateFormat;
 import java.text.NumberFormat;
@@ -31,6 +34,32 @@ public class Util {
       date = new Date();
     }
     return dateFormat.format(date);
+  }
+
+  /**
+   * Вытягиваем name из объекта
+   *
+   * @param model
+   * @return name или пустая строка
+   */
+  public static String name(BaseModel model) {
+    if (model != null) {
+      return model.name();
+    }
+    return StringUtils.EMPTY;
+  }
+
+  /**
+   * Вытягиваем id из объекта
+   *
+   * @param model
+   * @return id или пустая строка
+   */
+  public static String id(BaseModel model) {
+    if (model != null) {
+      return model.id();
+    }
+    return StringUtils.EMPTY;
   }
 
 }
