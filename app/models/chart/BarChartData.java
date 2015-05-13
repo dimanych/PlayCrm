1e51df760314b18ba8af3067c60b76b94ab95d91
Dmitry Grigoriev <stonger@yandex.ru>
2015-05-13 17:20:50 +0300

    Графики. Обновление

---
+++ app/models/chart/BarChartData.java
@@ -0,0 +1,76 @@
+package models.chart;
+
+import java.util.List;
+
+/**
+ * <p>Класс данных для построения графиков Bar Chart</p>
+ *
+ * @author Dmitriy Grigoriev
+ */
+public class BarChartData {
+  private String label;
+  private String fillColor;
+  private String strokeColor;
+  private String highlightFill;
+  private String highlightStroke;
+  private List<Integer> data;
+
+  public BarChartData(String label, String fillColor, String strokeColor, String highlightFill,
+                      String highlightStroke, List<Integer> data)
+  {
+    this.label = label;
+    this.fillColor = fillColor;
+    this.strokeColor = strokeColor;
+    this.highlightFill = highlightFill;
+    this.highlightStroke = highlightStroke;
+    this.data = data;
+  }
+
+  public String getLabel() {
+    return label;
+  }
+
+  public void setLabel(String label) {
+    this.label = label;
+  }
+
+  public String getFillColor() {
+    return fillColor;
+  }
+
+  public void setFillColor(String fillColor) {
+    this.fillColor = fillColor;
+  }
+
+  public String getStrokeColor() {
+    return strokeColor;
+  }
+
+  public void setStrokeColor(String strokeColor) {
+    this.strokeColor = strokeColor;
+  }
+
+  public String getHighlightFill() {
+    return highlightFill;
+  }
+
+  public void setHighlightFill(String highlightFill) {
+    this.highlightFill = highlightFill;
+  }
+
+  public String getHighlightStroke() {
+    return highlightStroke;
+  }
+
+  public void setHighlightStroke(String highlightStroke) {
+    this.highlightStroke = highlightStroke;
+  }
+
+  public List<Integer> getData() {
+    return data;
+  }
+
+  public void setData(List<Integer> data) {
+    this.data = data;
+  }
+}
