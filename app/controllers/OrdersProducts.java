348641a2d5d3b649e067d1c8c7d23f902e087afc
Dmitry Grigoriev <stonger@yandex.ru>
2015-05-27 23:16:48 +0300

    Заказы-Продукты, обновление

--- app/controllers/OrdersProducts.java
+++ app/controllers/OrdersProducts.java
@@ -3,8 +3,11 @@ package controllers;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import models.submodels.OrderProduct;
+import play.data.Form;
 import play.mvc.Controller;
 import play.mvc.Result;
+
+import static play.data.Form.form;
 
 /**
  * <p></p>
@@ -22,6 +25,20 @@ public class OrdersProducts extends Controller {
       ObjectNode result = play.libs.Json.newObject();
       result.put("id", id);
       result.put("name", name);
+      return ok(result);
+    } catch (Exception ex) {
+      return badRequest(ex.getMessage());
+    }
+  }
+
+  public static Result add() {
+    try {
+      ObjectNode result = play.libs.Json.newObject();
+      Form<OrderProduct> orderProductForm = form(OrderProduct.class).bindFromRequest();
+      if (orderProductForm.hasErrors()) {
+        throw new Exception("Не удалось завершить операцию =(");
+      }
+      orderProductForm.get().save();
       return ok(result);
     } catch (Exception ex) {
       return badRequest(ex.getMessage());
