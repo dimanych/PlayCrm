8c9fd3afc8e9a60fa3377e6d08c3795a33696db7
Dmitry Grigoriev <stonger@yandex.ru>
2015-05-27 17:58:37 +0300

    Рабочее ajax-удаление элементов Продукты в Сделках

--- app/controllers/OrdersProducts.java
+++ app/controllers/OrdersProducts.java
@@ -1,5 +1,6 @@
 package controllers;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import models.submodels.OrderProduct;
 import play.mvc.Controller;
@@ -15,14 +16,15 @@ public class OrdersProducts extends Controller {
   public static Result delete(Long id) {
     try {
       OrderProduct orderProduct = OrderProduct.findById(id);
-      String name = orderProduct.getProduct().name();
       orderProduct.delete();
+      JsonNode json = request().body().asJson();
+      String name = json.findPath("name").textValue();
       ObjectNode result = play.libs.Json.newObject();
       result.put("id", id);
       result.put("name", name);
       return ok(result);
-    } catch (Error er) {
-      return badRequest(er.getMessage());
+    } catch (Exception ex) {
+      return badRequest(ex.getMessage());
     }
   }
 }
